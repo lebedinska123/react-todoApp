55e2a750982516b0aa092716fd6b10fec0edb7b5
lebedinska123 <lebedinska28@mail.ru>
2020-04-22 00:20:49 +0300

    Updates

---
+++ precache-manifest.d0b85224dc1e68e0fb0ebe594a2a270c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5ce7275d23ef755a79f1c44857ca1e",
+    "url": "/react-todoApp/index.html"
+  },
+  {
+    "revision": "211f3326249b91d430e4",
+    "url": "/react-todoApp/static/css/main.3cdb75c1.chunk.css"
+  },
+  {
+    "revision": "79bc1ee2cfd23fd7cae4",
+    "url": "/react-todoApp/static/js/2.557f9360.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todoApp/static/js/2.557f9360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d531a771d0d803332d3",
+    "url": "/react-todoApp/static/js/3.854fdea8.chunk.js"
+  },
+  {
+    "revision": "211f3326249b91d430e4",
+    "url": "/react-todoApp/static/js/main.90e7b6a0.chunk.js"
+  },
+  {
+    "revision": "49ae86d84d6d13b5b344",
+    "url": "/react-todoApp/static/js/runtime-main.3234e157.js"
+  }
+]);
